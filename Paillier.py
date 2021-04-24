dbda5abfc25837448fbd636df0c577d94f9076e6
Brent <bdelia123@gmail.com>
2021-04-23 19:21:24 -0500

    Main Code
    Cleaned up, commented, and finished our base code.

--- Paillier.py
+++ Paillier.py
@@ -1,80 +1,68 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 23 14:58:37 2021
+import random
 
-@author: Travis
-"""
 
+# encrypts message 'm'
+#   m = message
+#   n = p * q (generated prime numbers)
+#   g = random int from 1 to n^2
 def encrypt(m, n, g):
-    r = 13
-    c = (pow(g,m) * pow(r,n)) % pow(n,2)
+    # generate random number 'r' from 1 to n-1, that satisfies gcd(r, n) = 1
+    r = random.randint(1, n-1)
+    while gcd(r, n) != 1:
+        r = random.randint(1, n-1)
+    c = (pow(g, m) * pow(r, n)) % pow(n, 2)
     return c
 
+
+# decrypts messsage 'c'
+#   c = encrypted message
+#   n = p * q
+#   mu = modular multiplicative inverse
+#   sig = lcm(p-1, q-1)
 def decrypt(c, n, mu, sig):
-    m = L_Func(pow(c,sig,pow(n,2)),n) * mu % n
+    m = L_Func(pow(c, sig, pow(n, 2)), n) * mu % n
     return m
 
+
+# x = g^sig % n2
+# n = p * q
 def L_Func(x, n):
     output = (x - 1)/n
     return output
 
 
+# if n is prime, return true
+def is_prime(n):
+    count = 0
+    for i in range(int(n/2)):
+        if n % (i+1) == 0:
+            count = count+1
+    return count == 1
+
+
+# calculate least common multiple between x and y
 def compute_lcm(x, y):
 
-   # choose the greater number
-   if x > y:
-       greater = x
-   else:
-       greater = y
+    # choose the greater number
+    if x > y:
+        greater = x
+    else:
+        greater = y
 
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
 
-   return lcm
-
-
-p = 19
-q = 23
-
-n = p * q
-n2 = pow(n,2)
-
-sig = compute_lcm(p-1, q-1)
-
-g = n+1
-
-Lg = L_Func(pow(g, sig, n2), n)
-Lg = int(Lg)
-
-mu = pow(Lg, -1, n)
-m1 = 123
-m2 = 124
-m3=125
+    return lcm
 
 
-c1 = encrypt(m1, n, g)
-print(str(c1))
-
-c2 = encrypt(m2, n, g)
-print(str(c2))
-
-c3 = encrypt(m3, n, g)
-print(str(c3))
-
-
-c1add2 = encrypt(m1+m2, n, g)
-print(c1add2)
-
-print(str(m1+m2))
-addmessage = decrypt(c1add2, n, mu, sig)
-print(addmessage)
-
-
-indivenc = c1 * c2
-indidec = decrypt(indivenc, n, mu, sig)
-print(str(indidec))
-print(str(m1 + m2))
-
+# calculate gcd between a and b
+def gcd(a, b):
+    if a < b:
+        return gcd(b, a)
+    elif a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
---
+++ main.py
@@ -0,0 +1,119 @@
+import paillier as plr
+import random
+
+
+# Paillier Cryptosystem -- Additive Homomorphic Encryption
+#   By: Travis Pope, Brent Delia, Torin Maguire, Jonnattan Deleon
+#
+#   This program utilizes an additive homomorphic encryption algorithm which federated learning
+#   utilizes. It applies differential privacy using this technique on the uploaded gradients
+#   during the training process to achieve the highest accuracy and prevents data from being
+#   compromised.
+#
+#   The additive homomorphic encryption technique essentially performs the additivity on
+#   multiple cipher-texts and decrypts the encrypted results at the same time. Thus, local
+#   users can send encrypted data for processing on a cloud server without revealing any info.
+
+
+# input validator function
+def getInput(n):
+    val = 0
+    msg = "Enter a numeric value for message #{0} (type 'exit' to quit): ".format(
+        n)
+    while val <= 0:
+        try:
+            val = input(msg)
+            if val == "exit":
+                return "exit"
+            val = int(val)
+            if val <= 0:
+                print("val must be > 0")
+        except ValueError:
+            print("value must be a valid integer")
+    return val
+
+
+# load any input to run the paillier algorithm
+def load_paillier_test(msg1, msg2):
+
+    # generate random (can be large if needed) prime numbers
+    primes = [i for i in range(1, 1000) if plr.is_prime(i)]
+    p = random.choice(primes)
+    q = random.choice(primes)
+    # prime numbers must be independent of each other
+    while p == q or plr.gcd(p*q, (p-1)*(q-1)) != 1:
+        q = random.choice(primes)
+
+    # print("Random primes generated:", "p =", p, "q =", q)
+
+    n = p * q
+    n2 = pow(n, 2)
+
+    # compute least common multiple between both primes
+    sig = plr.compute_lcm(p-1, q-1)
+
+    # generate a random integer from 0 < g < n2
+    g = random.randint(1, n2-1)
+
+    # apply modular multiplicative inverse
+    Lg = plr.L_Func(pow(g, sig, n2), n)
+    Lg = int(Lg)
+    mu = pow(Lg, -1, n)
+
+    # output the encrypted/decrypted results of message 1
+    msg1_enc = plr.encrypt(msg1, n, g)
+    print("Encrypted Message 1:", msg1_enc)
+    msg1_dec = plr.decrypt(msg1_enc, n, mu, sig)
+    print("Decrypted Message 1:", msg1_dec)
+
+    # output the encrypted/decrypted results of message 2
+    msg2_enc = plr.encrypt(msg2, n, g)
+    print("Encrypted Message 2:", msg2_enc)
+    msg2_dec = plr.decrypt(msg2_enc, n, mu, sig)
+    print("Decrypted Message 2:", msg2_dec)
+
+    print()
+    print("Additive Homomorphic Property states that:")
+    print("   D(E(m1 + m2)) == m1 * m2")
+    print()
+
+    # output the encrypted/decrypted results of message1 + message2
+    msg_added_enc = plr.encrypt(msg1 + msg2, n, g)
+    print("E(m1 + m2) =", msg_added_enc)
+    msg_added_dec = plr.decrypt(msg_added_enc, n, mu, sig)
+    print("D(E(m1 + m2)) =", msg_added_dec)
+
+    # output the encrypted/decrypted results of E(message1) * E(message2)
+    msg_mult_enc = msg1_enc * msg2_enc
+    print("E(m1) * E(m2) =", msg_mult_enc)
+    msg_mult_dec = plr.decrypt(msg_mult_enc, n, mu, sig)
+    print("m1 * m2 =", msg_mult_dec)
+
+    # check to see if the results satisfy the Homomorphic Additive Property which Federated Learning utilizes
+    if msg_added_dec == msg_mult_dec:
+        print()
+        print(msg_added_dec, "==", msg_mult_dec)
+        print("Homomorphic Additive Property applies, as also seen in Federated Learning")
+
+
+def main():
+    print()
+    print("Paillier Cryptosystem -- Additive Homomorphic Encryption")
+    print()
+
+    while True:
+
+        msg1 = getInput(1)
+        if msg1 == "exit":
+            break
+
+        msg2 = getInput(2)
+        if msg2 == "exit":
+            break
+
+        load_paillier_test(msg1, msg2)
+        print()
+
+
+if __name__ == '__main__':
+    main()
